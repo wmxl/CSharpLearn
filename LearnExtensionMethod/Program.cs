d15da4e1c5c80dfa12c484c59daebc3cb89b1a10
Yiming Zhang <yiming.zhang@thoughtworks.com>
2021-01-09 16:23:09 +0800

    maybe the last commit from this account

---
+++ LearnExtensionMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LearnExtensionMethod
+{
+    public static class MyExtensions
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] {' ', '.', '?'},
+                StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello Extension Methods";
+            int i = s.WordCount();
+            // same as this
+            // int i = MyExtensions.WordCount(s);
+            Console.WriteLine(i);
+        }
+        
+    }
+}
---
+++ Sona-repport/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Sona_repport
+{
+    public class Parent
+    {
+        public Parent()
+        {
+            DoSomething();  // Noncompliant
+        }
+
+        public virtual void DoSomething() // can be overridden
+        {
+            Console.WriteLine("Parent's DoSomething");
+        }
+    }
+
+    public class Child : Parent
+    {
+        private string foo;
+
+        public Child(string foo) // leads to call DoSomething() in Parent constructor which triggers a NullReferenceException as foo has not yet been initialized
+        {
+            this.foo = foo;
+        }
+
+        public override void DoSomething()
+        {
+            Console.WriteLine(this.foo.Length);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Child c = new Child("aaa");
+        }
+    }
+}
